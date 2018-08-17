f960f5ef558afce48b408c801e7b64e40e73ffda
Adeel Barki <adeelbarki@gmail.com>
2018-08-17 13:58:32 +0000

    1-main Projects

---
+++ projects/1-main/1-main.c
@@ -0,0 +1,8 @@
+#include "my_functions.h"
+
+int main(void)
+{
+
+	print_alphabets();
+	return (0);
+}
---
+++ projects/1-main/2-print_alphabets.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void print_alphabets(void)
+{
+	puts("abcdefghijklmnopqrstuvwxyz");
+}

db2cd1e389abae00b9b1d6ec64726c4b241770d7
Adeel Barki <adeelbarki@gmail.com>
2018-08-17 07:28:45 +0000

    3-main

---
+++ 0x01-main/3-main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int age;
+	char first_name_initial;
+	age = 98;
+	first_name_initial = 'A';
+	printf("Age = %d, Initial = %c\n", age, first_name_initial);
+	
+	age++;
+	printf("Age = %d, Initial = %c\n", age, first_name_initial);
+	age += 10;
+	printf("Age = %d, Initial = %c\n", age, first_name_initial);
+	age = age - 10;
+	printf("Age = %d, Initial = %c\n", age, first_name_initial);
+
+	printf("Char %c = %d\n", first_name_initial, first_name_initial);
+
+	first_name_initial++;
+	printf("Char %c = %d\n", first_name_initial, first_name_initial);
+	first_name_initial -= 3;
+	printf("Char %c = %d\n", first_name_initial, first_name_initial);
+	first_name_initial = first_name_initial + 10;
+	printf("Char %c = %d\n", first_name_initial, first_name_initial);
+	
+	return (0);
+}

e665eef747da941cd4c87a33df347a479bc7ae00
Adeel Barki <adeelbarki@gmail.com>
2018-08-18 14:37:52 +0000

    2-print_combination_2

---
+++ projects/0x03-loops/2-print_combination_2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void print_combination_2(void)
+{
+
+	int i, j;
+	for(i = 0; i < 10; i++)
+	{
+		for(j = 0; j < 10; j++)
+		{
+			if(i < j)
+			{
+				if(i != j)
+				{
+						printf("%d%d,", i, j);
+				}
+				
+			}
+		}
+	}
+}
+			

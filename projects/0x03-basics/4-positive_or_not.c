350daf408d29f5047f1326593fb46e862a0ea4da
Adeel Barki <adeelbarki@gmail.com>
2018-08-18 07:09:50 +0000

    4-main

---
+++ projects/0x03-basics/4-positive_or_not.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void positive_or_not(int n)
+{
+
+	if(n > 0){
+		printf("P");
+	}
+	if(n == 0){
+		printf("Z");
+	}
+	if(n < 0){
+		printf("N");
+	}
+		
+}
--- projects/0x03-basics/my_functions.h
+++ projects/0x03-basics/my_functions.h
@@ -1,2 +1,3 @@
 void print_base16(void);
 void positive_or_not(int n);
+void print_char(void);

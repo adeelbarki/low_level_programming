c992d0666e9c5e89d9d0721cdde20a4eb6a09d17
Adeel Barki <adeelbarki@gmail.com>
2018-08-18 07:23:28 +0000

    4-main

--- projects/0x03-basics/my_functions.h
+++ projects/0x03-basics/my_functions.h
@@ -1,3 +1,4 @@
+int print_char(char c);
 void print_base16(void);
 void positive_or_not(int n);
-void print_char(void);
+
--- projects/0x03-basics/print_char.c
+++ projects/0x03-basics/print_char.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
-void print_char(void)
+#include <unistd.h>
+
+int print_char(char c)
 {
 	
-
+	return (write(1, &c, 1));
 }
